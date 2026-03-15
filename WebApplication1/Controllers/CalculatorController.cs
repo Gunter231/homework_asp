95b1ffaef17f72097d44096b094ee2ef76133371
gunter231 <amir4563iu@gmail.com>
2026-03-15 23:49:44 +0300

    .

---
+++ WebApplication1/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult index(int a, int b)
+        {
+            return Content($"{a} + {b} = {a + b}");
+        }
+    }
+}
