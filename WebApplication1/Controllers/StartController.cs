4df8874bcbe1b20c2dd59ac8db6ba1f715ac3961
gunter231 <amir4563iu@gmail.com>
2026-03-15 23:34:41 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/StartController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StartController : Controller
+    {
+        public IActionResult hello()
+        {
+            DateTime currentTime = DateTime.Now;
+            int hour = currentTime.Hour;
+            string greeting;
+            if (hour >= 0 && hour < 6)
+            {
+                greeting = "Доброй ночи";
+            }
+            else if (hour >= 6 && hour < 12)
+            {
+                greeting = "Доброе утро";
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                greeting = "Добрый день";
+            }
+            else // hour >= 18 && hour < 24
+            {
+                greeting = "Добрый вечер";
+            }
+            Console.WriteLine(greeting);
+            return Content(greeting);
+        }
+    }
+}
